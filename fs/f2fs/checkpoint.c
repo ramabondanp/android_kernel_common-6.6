27a2e51978f7ca9260ec3abaefb579c56a420d96
lijiameng3 <lijiameng3@xiaomi.corp-partner.google.com>
2024-12-26 20:05:39 +0800

    ANDROID: vendor_hooks: export tracepoint symbols
    export trace_f2fs_gc_begin trace_f2fs_gc_end trace_f2fs_write_checkpoint trace_f2fs_sync_file_enter trace_f2fs_sync_file_exit trace_ufshcd_command so we can hook it in our ko to capture some ioinfo.
    
    Bug: 386161420
    Bug: 386866668
    Change-Id: I958cbf817345b8b44eb12cf3b37023d1281546ff
    Signed-off-by: lijiameng3 <lijiameng3@xiaomi.corp-partner.google.com>
    (cherry picked from commit c03da7c6088b6f14917787788807a608c86a9f97)

--- fs/f2fs/checkpoint.c
+++ fs/f2fs/checkpoint.c
@@ -21,6 +21,8 @@
 #include "iostat.h"
 #include <trace/events/f2fs.h>
 
+EXPORT_TRACEPOINT_SYMBOL_GPL(f2fs_write_checkpoint);
+
 #define DEFAULT_CHECKPOINT_IOPRIO (IOPRIO_PRIO_VALUE(IOPRIO_CLASS_BE, 3))
 
 static struct kmem_cache *ino_entry_slab;
